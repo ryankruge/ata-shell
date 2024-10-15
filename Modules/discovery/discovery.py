a24b2a0abbe142a3c162e5a76b4a7ec5a373acd9
Tomas <rkruge@gmail.com>
2024-10-15 23:30:53 +0100

    Implement Module Framework

---
+++ Modules/discovery/discovery.py
@@ -0,0 +1,59 @@
+from main import *
+
+
+REQUIRED = [ '-t', '-i' ]
+CONFIGURABLE = [ '-t', '-i' ]
+
+HELP_MESSAGE = f"""WOT ({colorama.Fore.RED}Who's Out There?{colorama.Fore.WHITE}):
+A simple tool for host discovery.
+
+[Required]:
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-t{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the target for the scan.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-i{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the desired network interface.
+
+[Optional]:
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-h{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Print the help display."""
+
+try:
+	if '-h' in sys.argv:
+		print(HELP_MESSAGE)
+		sys.exit()
+
+	if '-l' in sys.argv:
+		interfaces = get_if_list()
+		for interface in interfaces:
+			PrintMessage(f"{interface}")
+		sys.exit()
+
+	if not VerifyRequired(sys.argv, REQUIRED):
+		PrintMessage("Failure to verify provided flags. Please ensure you have included required criteria.")
+		sys.exit()
+
+	parameters = { '-t': None, '-i': None }
+	parameters = PopulateParameters(sys.argv, CONFIGURABLE, parameters)
+
+	discovery = Discovery(
+		parameters['-t'], 
+		parameters['-i']
+	)
+
+	hosts = discovery.GetHosts()
+	if not hosts:
+		PrintMessage("There was an error whilst attempting to scan the network.")
+
+	for host in hosts:
+		PrintMessage("Result for {}[{}{}{}]{}: {}({}{}{}){}".format(
+			colorama.Style.DIM,
+			colorama.Style.RESET_ALL,
+			host,
+			colorama.Style.DIM,
+			colorama.Style.RESET_ALL,
+			colorama.Style.DIM,
+			colorama.Style.RESET_ALL,
+			discovery.ReverseDNS(host),
+			colorama.Style.DIM,
+			colorama.Style.RESET_ALL
+		))
+except KeyboardInterrupt:
+	PrintMessage("Received halt signal. Exiting gracefully.")
+	sys.exit()
---
+++ Modules/discovery/main.py
@@ -0,0 +1,52 @@
+from scapy.all import ARP, srp, Ether, get_if_list
+import sys, colorama, socket
+
+class Discovery:
+	def __init__(self, target, interface):
+		self.target = target
+		self.interface = interface
+
+	def GetHosts(self):
+		packet = Ether(dst="FF:FF:FF:FF:FF:FF") / ARP(pdst=self.target)
+		replies = srp(packet, iface=self.interface, timeout=1, verbose=False)[0]
+
+		if not replies:
+			return []
+
+		addresses = []
+		for reply in range(0, len(replies)):
+			addresses.append(replies[reply][1].psrc)
+		return addresses
+
+	def ReverseDNS(self, host):
+		try:
+			hostname = socket.gethostbyaddr(host)[0]
+			return hostname
+		except socket.herror:
+			return
+
+def PrintMessage(message):
+	print("{}[{}*{}]{} {}".format(
+		colorama.Style.DIM,
+		colorama.Style.RESET_ALL,
+		colorama.Style.DIM,
+		colorama.Style.RESET_ALL,
+		message
+	))
+
+def PopulateParameters(arguments, flags, dictionary):
+	try:
+		temporary = dictionary
+		for argument in range(0, len(arguments)):
+			if arguments[argument] in flags:
+				dictionary[arguments[argument]] = arguments[argument + 1]
+		return temporary
+	except TypeError:
+		PrintMessage("There was an error whilst populating the parameter list.")
+		sys.exit()
+
+def VerifyRequired(arguments, required):
+	for argument in required:
+		if argument not in arguments:
+			return False
+	return True
